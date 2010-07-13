b867f787862dd3d933486dbcdbd24b193f724ff3
daniel.montero.martin@gmail.com <daniel.montero.martin@gmail.com@ec5ff7e2-0eba-98c3-61e4-52e84284cbdf>
2010-07-13 10:05:08 +0000

    Add RenderProject class in MogreGisProjects and change main program.

--- trunk/ mogregis3d/MainAppTest/Form1.cs
+++ trunk/ mogregis3d/MainAppTest/Form1.cs
@@ -24,41 +24,12 @@ namespace MainAppTest
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Project project = XmlSerializer.loadProject("Test1.xml");
+            RenderProject project = new RenderProject();
+            project.render2d("Test1.xml");
 
-            foreach (Source source in project.getSources())
-            {
-                BoundingBox envelope = new BoundingBox(0.0, 0.0, 1000.0, 1000.0);
-                FeatureDataSet ds = new FeatureDataSet();
-                source.DataSource.Open();
-                source.DataSource.ExecuteIntersectionQuery(envelope, ds);
-                source.DataSource.Close();
-
-                FeatureDataTable features = (FeatureDataTable)ds.Tables[0];
-
-                label1.Text = "Prueba:\n";
-
-                foreach (FeatureDataRow row in features)
-                {
-                    //imprimir alguna traza
-                    label1.Text = label1.Text;
-                    foreach (Object item in row.ItemArray)
-                        label1.Text += " -" + item;
-                    label1.Text += "\n";
-                }
-
-                //Show map
-                SharpMap.Map myMap = new SharpMap.Map(new Size(600, 300));
-                SharpMap.Layers.VectorLayer myLayer = new SharpMap.Layers.VectorLayer("mapa");
-                myLayer.DataSource = source.DataSource;
-                myMap.Layers.Add(myLayer);
-
-                myMap.ZoomToExtents();
-
-                pictureBox1.Image = myMap.GetMap();
-
-
-            }
+            label1.Text = project.getLabel();
+            project.getMap().Size = new Size(600,300);
+            pictureBox1.Image = project.getMap().GetMap();
         }
     }
 }
