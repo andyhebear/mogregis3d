ade7ab261ffefb01eb4c2c64a1767fad974aa9eb
pablo.h1989 <pablo.h1989@ec5ff7e2-0eba-98c3-61e4-52e84284cbdf>
2011-12-12 20:14:04 +0000

    added MogreApp.cs

---
+++ mogregis3d/MogreGisProjects/MogreApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mogre;
+
+namespace osgGISProjects
+{
+    public class MogreApp : Mogre.TutorialFramework.BaseApplication
+    {
+        //private Mogre.Root root;
+        //private Mogre.RenderWindow window;
+
+        public MogreApp()
+        {
+            this.Setup();
+        }
+
+        protected override void CreateScene()
+        {
+            mCamera.Position = new Vector3(-300, 500, 0);
+            mCamera.LookAt(0, 0, 0);
+        }
+
+        public SceneManager SceneManager
+        {
+            get { return mSceneMgr; }
+        }
+
+        public Root getRoot()
+        {
+            return this.mRoot;
+        }
+    }
+}
